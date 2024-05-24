caabc7a1f2be20b8486017b043d8427afa014ea4
AnLu996 <coar.andreacuela@gmail.com>
2024-05-24 08:53:16 -0500

    Se añade menú

--- main.cpp
+++ main.cpp
@@ -5,7 +5,42 @@
 
 int main()
 {
+    int opc = 1;
     int numBlocks = 5;
+    int bloque;
+    std::string contenido;
+
+    Interfaz interfaz(numBlocks, 10, 3);
+    do {
+        std::cout << "1. Leer bloque " <<std::endl;
+        std::cout << "2. Escribir bloque " <<std::endl;
+        std::cin >> opc;
+
+        switch (opc)
+        {
+        case 1: {
+            std::cin >> bloque;
+            interfaz.leerBloque(bloque);
+        }
+            break;
+        
+        case 2:{
+             
+            std::cin >> bloque;
+            std::cin >> contenido;
+            interfaz.escribirBloque(bloque,contenido);
+        }
+            break;
+        
+        default:
+            break;
+        }
+
+
+    } while (opc);
+
+
+    /*int numBlocks = 5;
     string contenido;
     Interfaz interfaz(numBlocks, 10, 3);
     for(int i=0; i<numBlocks; i++)
@@ -20,7 +55,7 @@ int main()
     for(int i=0; i<numBlocks; i++)
     {
         interfaz.leerBloque(i);
-    }
+    }*/
     cout<<"Misscount: "<<interfaz.getMissCount();
     cout<<", Hitcount: "<<interfaz.getHitcount()<<endl;
     return 0;
